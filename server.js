343b2446291da8633aa331b8d85de6f179934020
Gabriel <gabgithub05@gmail.com>
2022-01-23 04:44:17 +0100

    Revert "Changed list values to ghetti + js"
    This reverts commit 42685153a0d53fdc718f0d8840fb7ff78f579a55.

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ io.on('connection', socket => {
     socket.join(user.room);
 
     // Welcome current user
-    socket.emit('message', formatMessage(botName, 'Welcome to GhettiChat!'));
+    socket.emit('message', formatMessage(botName, 'Welcome to Fun-Talk!'));
 
     // Broadcast when a user connects
     socket.broadcast
